b4bb26ac9699188225962927b727a45e0f55ae8d
Jesus Hernandez <jjesusfhernandezc@gmail.com>
2023-09-06 19:33:24 -0400

    iniciado filtro de ToDos

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,21 @@
+import React from "react";
 import "./TodoSearch.css"
 
 function TodoSearch() {
+
+    const [searchValue, setSearchValue] = React.useState('');
+
+
     return (
         <input placeholder="Calcular facturacion"
-        className="TodoSearch" />
+        className="TodoSearch" 
+        value={searchValue}
+        onChange={(event)=> {
+                setSearchValue(event.target.value)
+                console.log(event.target.value)
+                
+            }}
+        />
     );
 }
 
